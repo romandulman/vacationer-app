3a9b333e546a794feb04bf9aab60c660860cb3d0
Roman <romandulman@gmail.com>
2019-08-25 21:11:07 +0300

    added code

---
+++ frontend/src/components/auth/Login.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.isLoggedIn,
+        showLogin: state.showLogin
+    };
+};
+
+const mapDispachToProps = dispach => {
+    return {
+        LoginConfirm: profile =>
+            dispach({ type: "IsLoggedIn", UserProfile: profile }),
+        handleShowLogin: () => dispach({ type: "LOGIN" })
+    };
+};
+class Login extends Component {
+
+    userLogin = {
+        UserName: "",
+        Password: ""
+    };
+
+    handleLogin = () => {
+        fetch("/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: this.userLogin.UserName,
+                password: this.userLogin.Password
+            })
+        })
+            .then(res => res.json())
+            .then(user => {
+                this.props.LoginConfirm(user.profile);
+            });
+    };
+
+
+    render() {
+        return (
+            <div>
+                <Dialog
+                    open={this.props.showLogin}
+                    onClose={this.props.showLogin}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">Add New Task</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            label="Username"
+                            type="text"
+                            onChange={e => {
+                                this.userLogin.UserName = e.target.value;
+                            }}
+                            fullWidth
+                        />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            label="Comment"
+                            type="text"
+
+                            onChange={e => {
+                                this.userLogin.Password = e.target.value;
+                            }}
+
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleView} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleLogin} color="primary">
+                            Publish Comment
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispachToProps
+)(Login);
