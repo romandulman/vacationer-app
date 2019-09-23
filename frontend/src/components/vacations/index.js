93d86cbe5870a2467564aa0f8766182135f2b020
Roman Dulman <romandulman@gmail.com>
2019-09-23 10:00:53 +0300

    cleaned code

--- frontend/src/components/vacations/index.js
+++
@@ -1,8 +0,0 @@
-import * as VacContainer from './Vacations.container'
-import VacReducer from './Vacations.reducers'
-import * as VacSaga from './Vacations.saga'
-import *  as VacService from './Vacations.services'
-
-export { Vacations as VacItemComponent } from "./Vacations.container";
-
-export default { VacReducer, VacContainer, VacSaga, VacService}
--- frontend/src/features/users.feature/index.js
+++ frontend/src/features/users.feature/index.js
@@ -1,6 +1,6 @@
 
 //import UserReducer from './User.reducers'
-export { UserLoginContainer as UserLogin } from "./UserLogin.container";
+export { UserLoginContainer as UserLogin } from "./user.containers/UserLogin.container";
 export { UserRegisterContainer as UserRegister } from "./UserRegister.contrainer";
 
 
