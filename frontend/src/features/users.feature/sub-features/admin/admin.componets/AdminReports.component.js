d23934a91961d0df7ceb131e27af5cafab3b6a5b
Roman <roman@gmail.com>
2019-10-23 01:20:19 +0300

    added code

--- frontend/src/features/users.feature/sub-features/admin/admin.componets/AdminReports.component.js
+++ frontend/src/features/users.feature/sub-features/admin/admin.componets/AdminReports.component.js
@@ -3,31 +3,13 @@ import CanvasJSReact from '../../../../../utils/canvasjs/canvasjs.react.js';
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core";
 import { Styles } from "../admin.assets/stylesheets/Admin.stylesheet";
-var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Reports extends Component{
-/*state ={
-    data:[
-        {
-            series: 8,
-            datums: 3,
-            dataType: 'ordinal'
-        },
 
-    ],
-    axes:[
-        { primary: true, type: 'ordinal', position: 'bottom' },
-        { position: 'left', type: 'linear', stacked: false }
-    ],
-    series:[
-       { type: 'bar'}
-    ]
-
-}*/
     render(){
         const { classes,followersData } = this.props;
-        const  data =  followersData.map(item => ( { label: `Vacation ID ${item.id}, Description:${item.description}`,  y: item.followerscount  }));
+        const  data =  followersData.map(item => ( { label: `Vacation ID: ${item.id}, Description: ${item.description}`,  y: item.followerscount  }));
 console.log(data)
         const options = {
             title: {
@@ -38,15 +20,9 @@ console.log(data)
                 {
                     type: "column",
                     dataPoints:data
-                   /*     { label: "Apple",  y: 10  },
-                        { label: "Orange", y: 15  },
-                        { label: "Banana", y: 25  },
-                        { label: "Mango",  y: 30  },
-                        { label: "Grape",  y: 28  }*/
-
                 }
             ]
-        }
+        };
         return(
             <div>
                 <div className={classes.chartDiv}>
