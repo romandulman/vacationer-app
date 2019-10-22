b1dba05c760b17941b4c119d1635675a6087e2ec
Roman <roman@gmail.com>
2019-10-22 13:32:45 +0300

    added code

--- frontend/src/features/users.feature/sub-features/admin/admin.componets/AdminReports.component.js
+++ frontend/src/features/users.feature/sub-features/admin/admin.componets/AdminReports.component.js
@@ -1,14 +1,36 @@
 import React,{Component} from 'react'
-
+import { Chart } from 'react-charts'
 
 class Reports extends Component{
-
+state ={
+    data:[
+        {
+            label: 'Series 1',
+            data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+        },
+        {
+            label: 'Series 2',
+            data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+        }
+    ],
+    axes:[
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+    ]
+}
     render(){
-
         return(
 
             <div>
                 <h1>Reports</h1>
+                <div
+                    style={{
+                        width: '400px',
+                        height: '300px'
+                    }}
+                >
+                    <Chart data={this.state.data} axes={this.state.axes} />
+                </div>
             </div>
 
 
